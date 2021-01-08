ab0c9d33775a3eae5a3aafe983b16be7ba7821b3
Shivashankar-gitH <shiva1894300@gmail.com>
2021-01-08 16:05:05 +0530

    cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero1 from "./Components/Hero1/Hero1.jsx";
 import Hero2 from "./Components/Hero2/Hero2.jsx";
 import Hero3 from "./Components/Hero3/Hero3.jsx";
+import Hero4 from "./Components/Hero4/Hero4";
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Hero1 />
 			<Hero2 />
 			<Hero3 />
+			<Hero4 />
 		</div>
 	);
 }
---
+++ src/Components/Hero4/Hero4.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Hero4.css";
+
+const Hero4 = () => {
+	return (
+		<div className="Hero4">
+			<div className="Hero4--in">
+				<div className="Hero4--tag1">
+					<h1 className="Hero4--tag1__in">A BIG OFFER FOR THIS SUMMER</h1>
+				</div>
+				<div className="Hero4--tag2">
+					<h1 className="Hero4--tag2__in">50% Off</h1>
+				</div>
+				<div className="Hero4--tag3">
+					<p className="Hero4--tag3__in">
+						There are many variations of passages of lorem Ipsum available, but
+						the majority have suffered alteration.
+					</p>
+				</div>
+				<div className="Hero4--btn">
+					<button className="Hero4--btn__in">JOIN NOW</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero4;
