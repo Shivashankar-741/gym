ad2f40803039b2b8f7e6faf957d3f7c3683e6d9f
Shivashankar-gitH <shiva1894300@gmail.com>
2021-01-08 13:58:02 +0530

    gym-cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Hero1 from "./Components/Hero1/Hero1.jsx";
+import Hero2 from "./Components/Hero2/Hero2.jsx";
 
 function App() {
 	return (
 		<div className="App">
 			<Hero1 />
+			<Hero2 />
 		</div>
 	);
 }
---
+++ src/Components/Hero2/Hero2.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Hero2.css";
+
+const Hero2 = () => {
+	return (
+		<div className="Hero2">
+			<div className="Hero2--girl">
+				<div className="Hero2--girl__tag">
+					<h2 className="Hero2--girl__tag1">FOR GIRL</h2>
+					<p className="Hero2--girl__tag2">
+						There are many variations of passages of lorem Ipsum available, but
+						the majority have suffered alteration.
+					</p>
+					<div className="Hero2--btn">
+						<button className="Hero2--btn__in">JOIN NOW</button>
+					</div>
+				</div>
+			</div>
+			<div className="Hero2--boys">
+				<div className="Hero2--boys__tag">
+					<h2 className="Hero2--boys__tag1">FOR BOYS</h2>
+					<p className="Hero2--boys__tag2">
+						There are many variations of passages of lorem Ipsum available, but
+						the majority have suffered alteration.
+					</p>
+					<div className="Hero2--btn">
+						<button className="Hero2--btn__in">JOIN NOW</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero2;
