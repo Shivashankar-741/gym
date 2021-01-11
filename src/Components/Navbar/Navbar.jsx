34c9bb35943df631c06cdfd8986a43e1af1d884a
Shivashankar-gitH <shiva1894300@gmail.com>
2021-01-11 19:17:09 +0530

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import gymLogo from "../../assets/gymlogo.png";
 import "./Navbar.css";
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const Navbar = () => {
+	const [click, setClick] = React.useState(false);
 	return (
 		<div className="Navbar">
-			<div className="Navbar--logo">
-				<img src={gymLogo} alt="gymlogo" className="Navbar--logo__in" />
+			<div className="nav-1">
+				<div className="Navbar--logo">
+					<img src={gymLogo} alt="gymlogo" className="Navbar--logo__in" />
+				</div>
+				<div
+					className="menu-icon"
+					onClick={() => {
+						setClick(!click);
+					}}
+				>
+					{click ? <FaTimes /> : <FaBars />}
+				</div>
 			</div>
-			<ul className="Navbar--list">
+			<ul className={click ? "Navbar--list " : "Navbar--list active"}>
 				<li className="Navbar--list__in">
-					<a href="">HOME</a>
+					<a href="#home">HOME</a>
 				</li>
 				<li className="Navbar--list__in">
 					<a href="#about">AB0UT</a>
@@ -28,12 +40,12 @@ const Navbar = () => {
 					<a href="#blog">BLOG</a>
 				</li>
 				<li className="Navbar--list__in">
-					<a href="">CONTACT</a>
+					<a href="#contact">CONTACT</a>
 				</li>
 			</ul>
-			<div className="Navbar--btn">
+			{/* <div className="Navbar--btn">
 				<button className="Navbar--btn__in">JOIN US</button>
-			</div>
+			</div> */}
 		</div>
 	);
 };
